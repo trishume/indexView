19b2e8f8bc1b9f9d6876aea7464805d7574aeda4
Tristan Hume <tris.hume@gmail.com>
2014-05-19 10:48:57 -0400

    Added better data set handling

--- js/main.js
+++ js/main.js
@@ -23,11 +23,43 @@ var Stats = {
     var doubled = Math.log(totalGrowth)/Math.log(2);
     var text = String(doubled.toFixed(2));
     return ["Times Doubled", text];
+  },
+  average: function(data, start, end) {
+    var total = 0;
+    for (var i = start; i < end; i++) {
+      total += data[i];
+    }
+    var average = total / (end-start);
+    var text = String((average*100).toFixed(2)) + '%';
+    return ["Average", text];
   }
 };
 
-var defaultStats = [Stats.averageGrowth, Stats.dollarsNow, Stats.timesDoubled, Stats.totalGrowth];
+function reinvestDividends(struct) {
+  var newData = [1.0];
+  var price = struct["price"];
+  var dividend = struct["dividend"];
+
+  for (var i = 1; i < price.length; i++) {
+    var grownVal = (price[i]/price[i-1]);
+    grownVal += (dividend[i]/12.0)/price[i];
+    grownVal *= newData[i-1];
+    newData[i] = grownVal;
+  };
+
+  return newData;
+}
+
+var defaultStats = [Stats.totalGrowth, Stats.averageGrowth, Stats.dollarsNow, Stats.timesDoubled];
 var dataSets = [
+  {
+    name: "S&P500 (Reinvested Dividends)",
+    notes: "Historical S&P500 data from <a href='http://www.econ.yale.edu/~shiller/data.htm'>Robert Shiller</a>. Not inflation adjusted. Includes reinvested dividends.",
+    file: "shiller_absolute.json",
+    statFuncs: defaultStats,
+    startYear: function(struct) {return struct["start"];},
+    datFunc: reinvestDividends
+  },
   {
     name: "S&P500",
     notes: "Historical S&P500 data from <a href='http://www.econ.yale.edu/~shiller/data.htm'>Robert Shiller</a>. Not inflation adjusted. Dividends not reinvested.",
@@ -39,27 +71,58 @@ var dataSets = [
     }
   },
   {
-    name: "S&P500 (Reinvested Dividends)",
+    name: "Inflation adjusted S&P500 (Reinvested Dividends)",
     notes: "Historical S&P500 data from <a href='http://www.econ.yale.edu/~shiller/data.htm'>Robert Shiller</a>. Not inflation adjusted. Includes reinvested dividends.",
-    file: "shiller_absolute.json",
+    file: "shiller_real.json",
+    statFuncs: defaultStats,
+    startYear: function(struct) {return struct["start"];},
+    datFunc: reinvestDividends
+  },
+  {
+    name: "Inflation adjusted S&P500",
+    notes: "Historical S&P500 data from <a href='http://www.econ.yale.edu/~shiller/data.htm'>Robert Shiller</a>. Inflation adjusted. Dividends not reinvested.",
+    file: "shiller_real.json",
     statFuncs: defaultStats,
     startYear: function(struct) {return struct["start"];},
     datFunc: function(struct) {
-      var newData = [1.0];
-      var price = struct["price"];
-      var dividend = struct["dividend"];
-
-      for (var i = 1; i < price.length; i++) {
-        var grownVal = (price[i]/price[i-1]);
-        grownVal += (dividend[i]/12.0)/price[i];
-        grownVal *= newData[i-1];
-        newData[i] = grownVal;
-      };
-
+      return struct["price"];
+    }
+  },
+  {
+    name: "S&P500 Dividend Yield",
+    notes: "Historical S&P500 data from <a href='http://www.econ.yale.edu/~shiller/data.htm'>Robert Shiller</a>.",
+    file: "shiller_absolute.json",
+    statFuncs: [Stats.average],
+    startYear: function(struct) {return struct["start"];},
+    datFunc: function(struct) {
+      var newData = [];
+      for (var i = 0; i < struct["price"].length; i++) {
+        newData[i] = struct["dividend"][i]/struct["price"][i];
+      }
       return newData;
     }
   },
 ];
+var dataFileCache = {};
+
+function loadDataFile(fileName) {
+  if(dataFileCache[fileName]) {
+    return dataFileCache[fileName];
+  }
+
+  xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", "datasets/"+fileName, false);
+  xmlhttp.send();
+
+  if(xmlhttp.status == 200) {
+    var json = JSON.parse(xmlhttp.responseText);
+    dataFileCache[fileName] = json;
+    return json;
+  } else {
+    alert("Failed to get "+fileName);
+    return null;
+  }
+}
 
 function loadSetChooser() {
   var select = $('dataset-chooser');
@@ -78,8 +141,9 @@ function setChooserChanged() {
 
 function loadData(setNum) {
   var set = dataSets[setNum];
-  var startYear = set.startYear(spData);
-  var data = set.datFunc(spData);
+  var struct = loadDataFile(set.file);
+  var startYear = set.startYear(struct);
+  var data = set.datFunc(struct);
 
   $('dataset-notes').innerHTML = set.notes;
 
