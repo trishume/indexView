ac403dc2712ed43d4363cf2e25ceaae7312536c9
Tristan Hume <tris.hume@gmail.com>
2014-05-31 19:40:23 -0400

    Real month selectors

--- js/dateField.js
+++ js/dateField.js
@@ -6,11 +6,30 @@ function DateField(element, delegate) {
 
 DateField.prototype = {
   initDom: function() {
+    this.elem.className = "date-entry"
+
     this.yearField = document.createElement("input");
     this.yearField.type = "text";
     this.yearField.className = "time-field";
     this.yearField.onchange = this.yearChanged.bind(this);
     this.elem.appendChild(this.yearField);
+
+    var monthContainer = document.createElement("span");
+    monthContainer.className = "date-months"
+    var months = "JFMAMJJASOND";
+    this.monthSelectors = [];
+    for (var i = 0; i < months.length; i++) {
+      var a = document.createElement("a");
+      a.href = "#";
+      a.appendChild(document.createTextNode(months[i]));
+
+      var monthChanged = this.monthChanged.bind(this);
+      a.onclick = monthChanged;
+
+      this.monthSelectors[i] = a;
+      monthContainer.appendChild(a);
+    };
+    this.elem.appendChild(monthContainer);
   },
 
   setDateFrom: function(month, startYear) {
@@ -20,7 +39,25 @@ DateField.prototype = {
   },
 
   setDate: function(year, month) {
+    this.curMonth = month;
     this.yearField.value = year;
+
+    for (var i = 0; i < this.monthSelectors.length; i++) {
+      var cls = (i==month) ? "cur-month" : "";
+      this.monthSelectors[i].className = cls;
+    };
+  },
+
+  monthChanged: function(event) {
+    var month = 0;
+    for (var i = 0; i < this.monthSelectors.length; i++) {
+      if(event.currentTarget == this.monthSelectors[i]) {
+        month = i;
+      }
+    };
+
+    this.curMonth = month;
+    this.delegate.fieldChanged();
   },
 
   yearChanged: function() {
@@ -32,7 +69,7 @@ DateField.prototype = {
     if(year.length != 4 || isNaN(year)) return null;
     year = parseInt(year);
     if(year < startYear) return null;
-    var month = (year - startYear)*12;
+    var month = (year - startYear)*12 + this.curMonth;
     if(month > maxMonth) return null;
     return month;
   }
