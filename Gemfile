2f8109046c79332eef911fa50f24bcd96e323fc6
Tristan Hume <tris.hume@gmail.com>
2014-07-28 11:04:14 -0400

    Optimization

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+gem 'jekyll'
+gem 'jekyll-assets'
+gem 'sass'
+gem 'uglifier'
---
+++ js/app.js
@@ -0,0 +1,7 @@
+//= require lib
+//= require dateField
+//= require statView
+//= require viewer
+//= require stats
+//= require data
+//= require main
